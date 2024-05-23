1b43d69a1d75844801ed25dd5df6f9000909d2ac
BiMia <150850183+BiMia@users.noreply.github.com>
2024-05-23 16:25:22 +0100

    initial-commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+    let intervalId = null;
+
+    function startColorChange() {
+      intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+      startButton.disabled = true;
+    }
+  
+    function stopColorChange() {
+      clearInterval(intervalId);
+      startButton.disabled = false;
+    }
+  
+    startButton.addEventListener('click', startColorChange);
+  
+    stopButton.addEventListener('click', stopColorChange);
+  });
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
